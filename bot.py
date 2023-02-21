45c690d9194b827f9c0b402cd6317d1f8d13cd3d
Kazanchev Nikita <KazanchevNikita89@yandex.ru>
2023-02-21 08:22:14 +0300

    adding planets

--- bot.py
+++ bot.py
@@ -1,28 +1,45 @@
 import logging
+
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
 import settings
+import ephem
 
-logging.basicConfig(filename='bot.log', level=logging.INFO)
+logging.basicConfig(format='%(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO,
+                    filename='bot.log')
 
-def greet_user(update, context):
+
+def greet_user(update, context):    
     print("Вызван /start")
     update.message.reply_text('Привет, пользователь!')
+
 
 def talk_to_me(update, context):
     user_text = update.message.text
     print(user_text)
     update.message.reply_text(user_text)
+
+
+def planets(update, context):
+    user_planet = update.message.text.split()
+    planet = getattr(ephem, user_planet)
+    constellation = ephem.compute(planet)
+    update.message.reply_text(constellation)
     
+
 def main():
     mybot = Updater(settings.API_KEY, use_context=True)
-
+    
     dp = mybot.dispatcher
     dp.add_handler(CommandHandler("start", greet_user))
+    dp.add_handler(CommandHandler("planet", planets))
     dp.add_handler(MessageHandler(Filters.text, talk_to_me))
+    
 
     logging.info("Бот стартовал")
     mybot.start_polling()
     mybot.idle()
 
-main()        
+if __name__ == '__main__':
+    main()        
